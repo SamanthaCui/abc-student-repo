3d4edeb48128d21e537b4285fa37b86343dc4a47
SAM🥑 <bcebdlbsjdngk@BcebdldeMacBook-Pro.local>
2020-09-29 10:39:43 +0800

    sss

--- lab-practice/Lab-3.1/js/script.js
+++ lab-practice/Lab-3.1/js/script.js
@@ -1,8 +1,21 @@
 let button = document. getElementById('button');
 let box = document. getElementById('box');
+let boxAngle = 0;
+let buttonAngle = 0;
 
 button.addEventListener("click",()=>{
 
-  box.style.transform = "rotate(360deg)"
+  boxAngle = boxAngle + 360;
+
+  box.style.transform = "rotate(" + boxAngle +"360deg)";
+
+})
+
+
+box.addEventListener("click",()=>{
+
+  buttonAngle = buttonAngle + 360;
+
+  button.style.transform = "rotate(" + buttonAngle +"360deg)";
 
 })
