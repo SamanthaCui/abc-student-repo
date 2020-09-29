40439a49ece2806b575918c39e33c367074c9545
SAM🥑 <bcebdlbsjdngk@BcebdldeMacBook-Pro.local>
2020-09-29 10:30:11 +0800

    2020/09/29

---
+++ lab-practice/Lab-3.1/js/script.js
@@ -0,0 +1,8 @@
+let button = document. getElementById('button');
+let box = document. getElementById('box');
+
+button.addEventListener("click",()=>{
+
+  box.style.transform = "rotate(360deg)"
+
+})
