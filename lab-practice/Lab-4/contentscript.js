5c65a142734348dff1ced11929779442d6140428
SAM🥑 <bcebdlbsjdngk@BcebdldeMacBook-Pro.local>
2020-10-21 12:40:51 +0800

    dd

--- lab-practice/Lab-4/contentscript.js
+++ lab-practice/Lab-4/contentscript.js
@@ -1,17 +1,27 @@
-console.log("I'm here on this website");
-
-
-function replace(word1,word2) {
-  let regEX = new RegExp(word1, "g");
-  document.body.innerHTML =   document.body.innerHTML.replace(regEX,word2);
-
+function repl(find, replace){
+  // info on the regular expression here: https://www.designcise.com/web/tutorial/how-to-replace-all-occurrences-of-a-word-in-a-javascript-string
+  var finder = new RegExp(find,"g");
+  // we replace the entire website's content with itself, but replace every occurence of
+  // the word that "finder" carries with the word that "replace" carries
+  // e.g. repl("Moon", "Potato")
+  document.body.innerHTML = document.body.innerHTML.replace(finder, replace);
 }
 
-replace("Moon","Potato");
 
-function gotMessage(request){
+// funtion to execute when any message arrives
+function gotMessage(request, sender, sendResponse){
+  // request includes the actual message
   console.log(request);
-  replace(message.word1,message.word2);
+
+  // use our replacement function to replace words as instructed by the popup page
+  repl(request.find, request.replace)
+
+  // sender lets us know who the message is fro (ie from our popup page)
+  // sendResponse is a function and allows us to reply to the sender (our popup page)
+  // sendResponse({message: "successfully replaced words!"});
+
 }
 
+// listening for messages:
 chrome.runtime.onMessage.addListener(gotMessage);
+// more on messaging: https://developer.chrome.com/extensions/messaging
