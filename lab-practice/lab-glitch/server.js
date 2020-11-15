72f2f08c1d0984181d2d48b0191ebc8d46f79240
SAM🥑 <bcebdlbsjdngk@BcebdldeMacBook-Pro.local>
2020-11-15 10:22:04 +0800

    glitch

---
+++ lab-practice/lab-glitch/server.js
@@ -0,0 +1,39 @@
+// console.log("byebyebye");
+//
+// let x = 1;
+// let y = 1;
+// console.log(x+y);
+
+
+const express = require('express'); //get the express code from node_modules(to where it was installed)
+const app = express();
+const port = 3000;
+
+//define routers behavior, what to return on which request
+
+
+// "/" route
+app.get('/', (req, res) => {
+  console.log("someone is looking at this site!");
+  // console.log(req);
+  // res.send('Hello World!');
+  res.sendFile(__dirname + '/sam/index.html');
+  console.log(req.query.time);
+    console.log(req.query.name);
+    // console.log(__dirname);
+    //So __dirname is a variable holding the absolute path of the project.
+    //ocalhost:3000/?name=sam&time=day
+})
+
+// "/sam" route
+app.get('/sam', (req, res) => {
+  console.log("someone is wishing to meet sam!");
+  res.send('hey sam');
+})
+
+
+// start server, listening to a specific port
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
