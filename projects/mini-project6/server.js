19d257bfcbb58aeb0a6fbac5af90cb69a8c0cf78
SAM🥑 <bcebdlbsjdngk@BcebdldeMacBook-Pro.local>
2020-11-18 20:48:12 +0800

    mini 6

--- projects/mini-project6/server.js
+++ projects/mini-project6/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000; //glitch: const port = process.env.PORT
+const port = process.env.PORT; //glitch: const port = process.env.PORT
 // const secret="blossom";
 
 let gifts = ['🌕'];
