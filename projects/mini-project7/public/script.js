daf9769898c7d104eb194a4764f41ff9bdef7971
SAM🥑 <bcebdlbsjdngk@BcebdldeMacBook-Pro.local>
2020-11-25 21:14:45 +0800

    7

---
+++ projects/mini-project7/public/script.js
@@ -0,0 +1,21 @@
+console.log("hi");
+
+let socket=io();
+let pic = document.getElementById('bubble');
+let face = document.getElementById('face');
+let  background= document.getElementById('wrapper');
+
+socket.on("user_number",(data)=>{
+  // user_number.innerHTML=data;
+  console.log(data);
+  if (data <=10){
+    pic.style.width = (179 * ((data-1) *0.2 + 1))+"px";
+    pic.style.height = (130 * ((data-1) *0.2 + 1))+"px";
+  }else if(data>10)
+  {
+    pic.style.display = "none";
+    // face.style.display = "none";
+  face.src = "img/boom.png";
+    document.body.style.backgroundColor = "rgb(250, 194, 202)";
+  }
+})
---
+++ projects/mini-project7/server.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+let user_online=0;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected',socket.id);
+  // number.push(socket.id);
+  user_online = user_online + 1;
+  io.emit("user_number",user_online);
+
+  socket.on('disconnect', () => {
+    console.log(socket.id,'user disconnected');
+    user_online = user_online - 1;
+    io.emit("user_number",user_online);
+  });
+  console.log(user_online);
+});
+
+
+
+
+http.listen(3330, () => {
+  console.log('listening on *:3000');
+});
