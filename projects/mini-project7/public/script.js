1adf537d086ee27687597d953076fad42142f12b
SAM🥑 <bcebdlbsjdngk@BcebdldeMacBook-Pro.local>
2020-11-26 10:45:08 +0800

    ,,,

--- projects/mini-project7/public/script.js
+++ projects/mini-project7/public/script.js
@@ -8,10 +8,10 @@ let  background= document.getElementById('wrapper');
 socket.on("user_number",(data)=>{
   // user_number.innerHTML=data;
   console.log(data);
-  if (data <=7){
+  if (data <=10){
     pic.style.width = (179 * ((data-1) *0.2 + 1))+"px";
     pic.style.height = (130 * ((data-1) *0.2 + 1))+"px";
-  }else if(data>7)
+  }else if(data>10)
   {
     pic.style.display = "none";
     // face.style.display = "none";
