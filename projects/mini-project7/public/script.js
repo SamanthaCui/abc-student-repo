3dc5955d6f332d6f90846171ed5bc76d6cbd5470
SAM🥑 <bcebdlbsjdngk@BcebdldeMacBook-Pro.local>
2020-11-27 19:33:41 +0800

    mini7

--- projects/mini-project7/public/script.js
+++ projects/mini-project7/public/script.js
@@ -11,85 +11,93 @@ let pic = document.getElementById('bubble');
 let face = document.getElementById('face');
 let button = document.getElementById('buttonID');
 let background= document.getElementById('wrapper');
+let press=false;
 
 pic.style.display = "none";
 face.style.display = "none";
-
 button.style.display = "block";
 
+
 socket.on("user_number",(data)=>{
+button.addEventListener("click", ()=>{
+  animation(data);
+  press = true;
+})
+})
 
-  // user_number.innerHTML=data;
-
-  // alert("button was clicked");
-  button.onclick = function() {
-    blowing.play();
-    console.log(data);
-    if (data <=10){
-      let scale = data *0.2 + 1;
-      let scale2 = ((data-1) *0.2 + 1);
-
-      document.getElementById("bubble").animate([
-        // keyframes
-        { transform: 'scale('+scale2 +')' },
-        { transform: 'scale('+scale +')'}
-      ], {
-        // timing options
-        duration: 1000,
-        iterations: 1,
-        fill: 'forwards'
-      });
-
-      // pic.style.width = (179 * ((data-1) *0.2 + 1))+"px";
-      // pic.style.height = (130 * ((data-1) *0.2 + 1))+"px";
-
-      pic.style.display = "block";
-      face.style.display = "block";
-      button.style.display = "none";
-      blowing.play();
-    }else if(data>10){
-      blowing.pause();
-      pop.play();
-      button.style.display = "none";
-      pic.style.display = "block";
-      face.style.display = "block";
-      face.src = "img/boom.png";
-
-      document.getElementById("bubble").animate([
-        // keyframes
-        { transform: 'scale(1.4)'},
-        { transform: 'scale(6)'}
-      ], {
-        // timing options
-        duration: 3000,
-        iterations: 1,
-        fill: 'forwards'
-      });
-
-      document.getElementById("bubble").animate([
-        // keyframes
-        { opacity: '1'},
-        { opacity: '0'}
-      ], {
-        // timing options
-        duration: 1000,
-        iterations: 1,
-        fill: 'forwards'
-      });
-
-      document.body.animate([
-        // keyframes
-        { backgroundColor:'white'},
-        { backgroundColor:'rgb(250, 194, 202)'}
-      ], {
-        // timing options
-        duration: 1000,
-        delay: 500,
-        iterations: 1,
-        fill: 'forwards'
-      });
-
-      // document.body.style.backgroundColor = "rgb(250, 194, 202)";
-    }
+socket.on("user_number",(data)=>{
+  if (press==true){
+    animation(data);
   }
 })
+
+function animation(data){
+  blowing.play();
+  console.log(data);
+  if (data <=10){
+    let scale = data *0.2 + 1;
+    let scale2 = ((data-1) *0.2 + 1);
+
+    document.getElementById("bubble").animate([
+      // keyframes
+      { transform: 'scale('+scale2 +')' },
+      { transform: 'scale('+scale +')'}
+    ], {
+      // timing options
+      duration: 1000,
+      iterations: 1,
+      fill: 'forwards'
+    });
+
+
+    pic.style.display = "block";
+    face.style.display = "block";
+
+    button.style.display = "none";
+    // pic.style.width = (179 * ((data-1) *0.2 + 1))+"px";
+    // pic.style.height = (130 * ((data-1) *0.2 + 1))+"px";
+  }else if(data>10){
+    blowing.pause();
+    pop.play();
+    button.style.display = "none";
+    pic.style.display = "block";
+    face.style.display = "block";
+    face.src = "img/boom.png";
+
+    document.getElementById("bubble").animate([
+      // keyframes
+      { transform: 'scale(1.4)'},
+      { transform: 'scale(6)'}
+    ], {
+      // timing options
+      duration: 3000,
+      iterations: 1,
+      fill: 'forwards'
+    });
+
+    document.getElementById("bubble").animate([
+      // keyframes
+      { opacity: '1'},
+      { opacity: '0'}
+    ], {
+      // timing options
+      duration: 1000,
+      iterations: 1,
+      fill: 'forwards'
+    });
+
+    document.body.animate([
+      // keyframes
+      { backgroundColor:'white'},
+      { backgroundColor:'rgb(250, 194, 202)'}
+    ], {
+      // timing options
+      duration: 1000,
+      delay: 500,
+      iterations: 1,
+      fill: 'forwards'
+    });
+
+    // document.body.style.backgroundColor = "rgb(250, 194, 202)";
+  }
+}
